d0cf70177b227ddd8355d22c2489fbcddfd4d701
Ubuntu <ubuntu@ip-172-31-34-57.ec2.internal>
2023-11-14 01:14:32 +0000

    updates

--- bin/perceptron.py
+++ bin/perceptron.py
@@ -1,14 +1,28 @@
+"""
+This is a class
+"""
 class Perceptron:
-  def train(self, inputs, labels):
-    dummied_inputs = [x + [-1] for x in inputs]
-    self._weights = [0.2] * len(dummied_inputs[0])
-    for _ in range(5000):
-      for input, label in zip(dummied_inputs, labels):
-        label_delta = (label - self.predict(input))
-        for index, x in enumerate(input):
-          self._weights[index] += .1 * x * label_delta
-  def predict(self, input):
-    if len(input) == 0:
-      return None
-    input = input + [-1]
-    return int(0 < sum([x[0]*x[1] for x in zip(self._weights, input)]))
+    """
+    This is perceptron
+    """
+    def __init__(self):
+        self._weights = 0
+    def train(self, inputs, labels):
+        """
+        This is train
+        """
+        dummied_inputs = [x + [-1] for x in inputs]
+        self._weights = [0.2] * len(dummied_inputs[0])
+        for _ in range(5000):
+            for input_iterable, label in zip(dummied_inputs, labels):
+                label_delta = (label - self.predict_function(input_iterable))
+                for index_iterable, x_iterable in enumerate(input_iterable):
+                    self._weights[index_iterable] += .1 * x_iterable * label_delta
+    def predict_function(self, input_def):
+        """
+        This is predict
+        """
+        if len(input_def) == 0:
+            return None
+        input_def = input_def + [-1]
+        return int(0 < sum([x[0]*x[1] for x in zip(self._weights, input_def)]))
