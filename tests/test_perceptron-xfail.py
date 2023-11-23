dff7f5095333857c25fc31b028540ca4fa96db8e
Ubuntu <ubuntu@ip-172-31-34-57.ec2.internal>
2023-11-23 17:09:13 +0000

    added testing files

--- tests/test_perceptron-xfail.py
+++ tests/test_perceptron-xfail.py
@@ -1,9 +1,11 @@
 import sys
+import pytest
 sys.path.append(".")
 
 from bin.perceptron import Perceptron
 
-def test_perceptron():
+@pytest.mark.xfail(reason = "Testing")
+def test_perceptron_xfail():
     the_perceptron = Perceptron()
     the_perceptron.train([
         [1,1],
@@ -12,7 +14,7 @@ def test_perceptron():
         [0,0],
     ], [1,1,1,0])
 
-    assert the_perceptron.predict_function([1,1]) ==  1, "1-1 failed"
+    assert the_perceptron.predict_function([1,1]) ==  100, "1-1 failed"
     assert the_perceptron.predict_function([1,0]) ==  1, "1-0 failed"
     assert the_perceptron.predict_function([0,1]) ==  1, "0-1 failed"
     assert the_perceptron.predict_function([0,0]) ==  0, "0-0 failed"
